5966f72bace47a124e36ee0410adbb3f8c7abdf6
Wang, Dawei <dwang@entertainment.com>
2016-04-22 12:27:47 -0400

    initial check in

---
+++ lib/puppet/provider/pulp_repo/repo.rb
@@ -0,0 +1,145 @@
+require 'json'
+Puppet::Type.type(:pulp_repo).provide(:repo) do
+  desc "Manage pulp repo"
+  commands :pulpadmin => 'pulp-admin'
+  commands :curl => 'curl'
+
+  def initialize(value={})
+  	super(value)
+  	@property_flush = {}
+  	#login fetch user certificate
+  	login_get_cert
+  end
+  mk_resources_methods
+
+  def self.instances
+  	repos =[]
+  	execpipe("#{curl} -k --cert ~/.pulp/user-cert.pem #{@resource[:server]}/#{@resources[:api_path]}/repositories/?details=true") do |output|
+	  	repo_raw_json= JSON.parse(output)
+	  	#An array returned
+	  	repo_json = repo_raw_json[0]
+
+	  	repo_json.each do |repo|
+	  		data_hash ={}
+	  		data_hash[:id] = repo['id']
+	  		data_hash[:display_name] = repo['display_name']
+	  		data_hash[:description] = repo['description']
+	  		data_hash[:server] = @resource[:server]
+	  		data_hash[:api_path] = @resource[:api_path]
+	  		if repo['importers']
+	  			repo['importers'].each do |importer|
+	  				if importer['config']
+	  					data_hash[:feed]=importer['config']['feed']
+	  				end
+	  			end
+	  		end
+
+	  		if repo['distributors']
+	  			repo['distributors'].each do |distributor|
+	  				if distributor['distributor_type_id'] == 'yum_distributor'
+	  					data_hash[:server_http] = distributor['config']['http']
+	  					data_hash[:server_https] = distributor['config']['https']
+	  				end
+	  			end
+	  		end
+	  		data_hash[:ensure] = 'present'
+	  		data_hash[:provider] = self.name
+	  		repos << data_hash unless data_hash.empty?
+	  	end
+  	end
+  	repos
+  end
+
+  def self.prefetch(repos)
+  	instances.each do |prov|
+  		if r = repos[prov.name]
+  			r.provider = prov
+  		end
+  	end
+  end
+
+  def exsits?
+  	@property_hash[:ensure] == :present
+  end
+
+  def create
+  	execoutput(repo_create_cmd)
+  	@property_hash[:ensure] = :present
+  rescue Puppet::ExecutionFailure => details
+  	raise Puppet::Error, "Cannot create repo : #{repo_create_cmd}"
+  end
+
+  def destroy
+  	execoutput(repo_delete_cmd)
+  	@property_hash.clear
+  rescue Puppet::ExecutionFailure => details
+  	raise Puppet::Error, "Cannot delete repo : #{repo_delet_cmd}"
+  end
+
+  def name=(value)
+  	@property_flush(:name) = value
+  end
+
+  def description=(value)
+  	@property_flush(:description) = value
+  end
+
+  def feed=(value)
+  	@property_flush(:feed) = value
+  end
+
+  def serve_http=(value)
+  	@property_flush(:serve_http) = value
+  end
+
+  def serve_https=(value)
+  	@property_flush(:serve_https) = value
+  end
+
+  def flush
+  	options=[]
+  	 if @property_flush
+  	 	 options << '--display-name' << @resources[:name] if @property_flush[:name]
+  	 	 options << '--discription' << @resources[:description] if @property_flush[:description]
+  	 	 options << '--feed' << @resources[:feed] if @property_flush[:feed]
+  	 	 options << '--serve-http' << @resources[:serve_http] if @property_flush[:serve_https]
+  	 	 options << '--serve-https' << @resources[:serve_http] if @property_flush[:serve_https]
+  	 end
+  	 unless options.empty?
+  	 	execoutput(repo_update_cmd(options))
+  end
+
+  private
+
+  def repo_create_cmd()
+  	repo_create=[command(:pulpadmin), "#{@resources[:type]}", "repo", "create" , "--repo-id", "#{@resources[:id]}" ]
+  	if @resources[:feed]
+  		repo_create = repoo_create + ["--feed", "#{@resources[:feed]}"]
+  	end
+  	if @resources[:serve_http]
+  		repo_create = repoo_create + ["--server-http", "#{@resources[:serve_http]}"]
+  	end
+  	if @resources[:serve_https]
+  		repo_create = repoo_create + ["--server-https", "#{@resources[:serve_https]}"]
+  	end
+  	repo_create
+  end
+
+  def repo_delete_cmd()
+  	[command(:pulpadmin), @resources[:type], "repo", "delete", "--bg", "--repo-id", @resources[:id]]
+  end
+
+  def repo_update_cmd(options)
+  	[command(:pulpadmin), @resources[:type], "repo", "update","--bg", "--repo-id", @resources[:id] ]+options
+  end
+  #assume user have ~/.pulp/admin setup with auth username and password
+  #[auth]
+  #user=
+  #pass=
+  def login_get_cert
+  	 execoutput("pulp-admin login")
+  rescue Puppet::ExecutionFailure => details
+  	raise Puppet::Error, "Check ~/.pulp/admin.conf for credentials, could not log in with pulpadmin: #{detail}"
+  end
+
+end
---
+++ lib/puppet/type/pulp_repo.rb
@@ -0,0 +1,80 @@
+require 'uri'
+Puppet::Type.newtype(:pulp_repo) do
+  @doc = <<-EOT
+  Resource type for pulp repository
+  EOT
+  ensurable do
+    desc <<-EOS
+      Register/unregister a pulp consumer.
+    EOS
+
+    newvalue(:present) do
+      provider.create
+    end
+
+    newvalue(:absent) do
+      provider.destroy
+    end
+
+    defaultto :present
+  end
+
+  newparam(:id) do
+    desc "uniquely identifies the consumer; only alphanumeric, ., -, and _ allowed"
+    isnamevar
+  end
+
+  newparam(:server) do
+    desc "url of the server like https://pulp.repo"
+    isnamevar
+  end
+
+  newparam(:api_path) do
+  	desc "api path"
+  	defaultto "pulp/api/v2"
+  end
+
+  newparam(:type) do
+  	newvalues(:rpm, :puppet)
+  	defautlto :rpm
+  end
+
+  newproperty(:name) do
+  	desc "Display name for repo"
+  	default to :id
+  end
+
+  newproperty(:description) do
+  	desc "Description of the repository"
+  	default to ''
+  end
+
+  newproperty(:feed) do
+  	desc "Feed for the repository"
+
+  	validate do |value|
+  		unless URI.parse(value).is_a?(URI::HTTP) ||
+  			URI.parse(value).is_a?(URI::HTTPS) ||
+  			URI.parse(value).scheme == 'file'
+  			fail("Invalid feed #{value}")
+  		end
+  	end
+  end
+
+  newproperty(:server_http) do
+  	desc "Server through http"
+  	newvalue(:true)
+  	newvalue(:false)
+
+  	defaultto :true
+  end
+
+  newproperty(:server_https) do
+  	desc "Server through http"
+  	newvalue(:true)
+  	newvalue(:false)
+
+  	defaultto :true
+  end
+
+end
